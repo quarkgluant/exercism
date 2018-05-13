b252be5269d42043e102743f90453364ae434244
Patrice Cadiot <pathibul.r@gmail.com>
2018-05-13 13:39:40 +0200

    delete 2 lines of nth_prime

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -6,8 +6,6 @@ class Prime
 
   def self.nth(rank_prime)
     raise ArgumentError if rank_prime < 1
-    return 2 if rank_prime == 1
-    return 3 if rank_prime == 2
     prime_array = [2]
     index = 1
     (3..(rank_prime**2)).each do |integer|
