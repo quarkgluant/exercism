40fed14b7f632a2240bf26c4608869e9da3d6817
Patrice Cadiot <pathibul.r@gmail.com>
2019-05-27 16:10:02 +0200

    add approved to Python Track hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,5 +2,4 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Strands must be equal length")
 
-    nucleotides = zip(list(strand_a), list(strand_b))
-    return sum(pairs[0] != pairs[1] for pairs in list(nucleotides))
+    return sum(base_a != base_b for base_a, base_b in zip(strand_a, strand_b))
