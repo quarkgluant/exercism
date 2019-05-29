8124c7a169396c988677fb069b43da9f5613ca78
Patrice Cadiot <pathibul.r@gmail.com>
2019-05-29 21:49:16 +0200

    add new raindrops to Python track

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,11 +1,7 @@
 def raindrops(number):
-    result = ""
-    if number % 3 == 0:
-        result += "Pling"
-    if number % 5 == 0:
-        result += "Plang"
-    if number % 7 == 0:
-        result += "Plong"
-    if not (number % 3 == 0 or number % 5 == 0 or number % 7 == 0):
-        result = f"{number}"
-    return result
+    connections = dict([(3, "Pling"), (5, "Plang"), (7, "Plong")])
+    result = [connections[div] for div in connections if number % div == 0]
+    if len(result) == 0:
+        return f"{number}"
+    else:
+        return "".join(result)
