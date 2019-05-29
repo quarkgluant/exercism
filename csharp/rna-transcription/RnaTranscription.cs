19c58ecb762e07dcc426bf89f77ccce7c9363059
Patrice Cadiot <pathibul.r@gmail.com>
2019-05-29 14:00:42 +0200

    add to C# track approved rna-transcription with IReadOnlyDictionary and .gitignore

--- csharp/rna-transcription/RnaTranscription.cs
+++ csharp/rna-transcription/RnaTranscription.cs
@@ -5,30 +5,20 @@ public static class RnaTranscription
 {
     public static string ToRna(string nucleotide)
     {
-        Transcription transcription = new Transcription();
+        IReadOnlyDictionary<char, char> transcription = new Dictionary<char, char>()
+        {
+            {'G', 'C'},
+            {'C', 'G'},
+            {'A', 'U'},
+            {'T', 'A'}
+        };
 
         StringBuilder rna = new StringBuilder(nucleotide);
         for (int index = 0; index < nucleotide.Length; index++)
         {
-            rna[index] = transcription.ToRna[nucleotide[index]];
+            rna[index] = transcription[nucleotide[index]];
         }
         return rna.ToString();
     }
-
-    public interface ITranscription
-    {
-        Dictionary<char, char> ToRna { get; }
-    }
-
-    public class Transcription : ITranscription
-    {
-        public Dictionary<char, char> ToRna
-            => new Dictionary<char, char>()
-                {
-                    {'G', 'C'},
-                    {'C', 'G'},
-                    {'A', 'U'},
-                    {'T', 'A'}
-                };
-    }
+    
 }
