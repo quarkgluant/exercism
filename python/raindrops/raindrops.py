6e504eeb58bd9d6bb2b0db066ec81688e0ba7653
Patrice Cadiot <pathibul.r@gmail.com>
2019-05-29 23:42:54 +0200

    change in raindrops in Python dict in OrderedDict

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,5 +1,7 @@
+from collections import OrderedDict
+
 def raindrops(number):
-    connections = dict([(3, "Pling"), (5, "Plang"), (7, "Plong")])
+    connections = OrderedDict([(3, "Pling"), (5, "Plang"), (7, "Plong")])
     result = [connections[div] for div in connections if number % div == 0]
     if len(result) == 0:
         return f"{number}"
