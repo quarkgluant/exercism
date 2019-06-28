e0a4d293e631ee4a8e5a073d61fd2e49e2bbcacc
Patrice Cadiot <pathibul.r@gmail.com>
2019-06-28 13:27:57 +0200

    refactor luhn in Ruby Track to avoid too many class method

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,30 +1,34 @@
 class Luhn
-  class << self
-
-    def valid?(input)
-      input_cleaned = input.delete(' ')
-      return false if input_cleaned.length < 2 || input_cleaned =~ /[^\d]/
-
-      checksum(digits(input_cleaned)) % 10 == 0
-    end
-
-    private
-
-    def double(digit)
-      if digit.nil?
-        0
-      else
-        (digit * 2 > 9) ? digit * 2 - 9 : digit * 2
-      end
-    end
-
-    def digits(input)
-      input.chars.reverse.map(&:to_i)
-    end
-
-    def checksum(digits)
-      digits.each_slice(2).sum { |first, second| first + double(second) }
-    end
-
+  def initialize(input)
+    @input = input.delete(' ')
   end
+
+  def self.valid?(input)
+    new(input).valid?
+  end
+
+  def valid?
+    return false if @input.length < 2 || @input =~ /[^\d]/
+
+    checksum % 10 == 0
+  end
+
+  private
+
+  def double(digit)
+    if digit.nil?
+      0
+    else
+      (digit * 2 > 9) ? digit * 2 - 9 : digit * 2
+    end
+  end
+
+  def digits
+    @input.chars.reverse.map(&:to_i)
+  end
+
+  def checksum
+    digits.each_slice(2).sum { |first, second| first + double(second) }
+  end
+
 end
