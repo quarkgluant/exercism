985815f6915e326517400d173d62c1e633f3c8c3
Patrice Cadiot <pathibul.r@gmail.com>
2019-08-12 13:52:20 +0200

    Update approved space-age in C# Track

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -3,40 +3,44 @@ using System.Collections.Generic;
 
 public class SpaceAge
 {
+    enum Planets
+    {
+        Earth, Mercury, Venus, Mars, Jupiter, Saturn, Uranus, Neptune
+    }
     private const double EARTH_YEAR_SECONDS = 31_557_600;
     
-    private IReadOnlyDictionary<string, Func<double, double>> PLANETS_PERIOD_FUNC = new Dictionary<string, Func<double, double>>()
+    private IReadOnlyDictionary<Planets, Func<double, double>> PLANETS_PERIOD_FUNC = new Dictionary<Planets, Func<double, double>>()
     {
-        { "earth", x => Math.Round(x, 2) },
-        { "mercury", x => Math.Round(x / 0.2408467, 2) },
-        { "venus", x => Math.Round(x  / 0.61519726, 2) },
-        { "mars", x => Math.Round(x / 1.8808158, 2) },
-        { "jupiter", x => Math.Round(x / 11.862615, 2) },
-        { "saturn", x => Math.Round(x / 29.447498, 2) },
-        { "uranus", x => Math.Round(x / 84.016846, 2) },
-        { "neptune", x => Math.Round(x / 164.79132, 2) }
+        { Planets.Earth, x => x },
+        { Planets.Mercury, x => x / 0.2408467 },
+        { Planets.Venus, x => x  / 0.61519726 },
+        { Planets.Mars, x => x / 1.8808158 },
+        { Planets.Jupiter, x => x / 11.862615 },
+        { Planets.Saturn, x => x / 29.447498 },
+        { Planets.Uranus, x => x / 84.016846 },
+        { Planets.Neptune, x => x / 164.79132 }
     };
 
-    public double AgeOnEarth { get; }
+    private double AgeOnEarth { get; }
     public SpaceAge(double seconds)
     {
         AgeOnEarth = seconds / EARTH_YEAR_SECONDS;
     }
     
-    public double OnEarth() => PLANETS_PERIOD_FUNC["earth"](AgeOnEarth);
+    public double OnEarth() => PLANETS_PERIOD_FUNC[Planets.Earth](AgeOnEarth);
 
-    public double OnMercury() => PLANETS_PERIOD_FUNC["mercury"](AgeOnEarth);
+    public double OnMercury() => PLANETS_PERIOD_FUNC[Planets.Mercury](AgeOnEarth);
 
-    public double OnVenus() => PLANETS_PERIOD_FUNC["venus"](AgeOnEarth);
+    public double OnVenus() => PLANETS_PERIOD_FUNC[Planets.Venus](AgeOnEarth);
 
-    public double OnMars() => PLANETS_PERIOD_FUNC["mars"](AgeOnEarth);
+    public double OnMars() => PLANETS_PERIOD_FUNC[Planets.Mars](AgeOnEarth);
     
-    public double OnJupiter() => PLANETS_PERIOD_FUNC["jupiter"](AgeOnEarth);
+    public double OnJupiter() => PLANETS_PERIOD_FUNC[Planets.Jupiter](AgeOnEarth);
 
-    public double OnSaturn() => PLANETS_PERIOD_FUNC["saturn"](AgeOnEarth);
+    public double OnSaturn() => PLANETS_PERIOD_FUNC[Planets.Saturn](AgeOnEarth);
 
-    public double OnUranus() => PLANETS_PERIOD_FUNC["uranus"](AgeOnEarth);
+    public double OnUranus() => PLANETS_PERIOD_FUNC[Planets.Uranus](AgeOnEarth);
 
-    public double OnNeptune() => PLANETS_PERIOD_FUNC["neptune"](AgeOnEarth);
+    public double OnNeptune() => PLANETS_PERIOD_FUNC[Planets.Neptune](AgeOnEarth);
     
 }
